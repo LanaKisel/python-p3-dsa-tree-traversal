53ad71f44944ddbb2ef718fdde1bc2790c9aa667
Lana Kisel <svetlana.kisel.ca@gmail.com>
2024-02-08 22:28:43 -0800

    tree.py

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,43 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+
+    while nodes_to_visit:
+      current = nodes_to_visit.pop()
+      if current['id'] == id:
+        return current
+      else:
+        nodes_to_visit = nodes_to_visit + current['children']
+    return None      
+    ### EXAMPLES
+  # def breadth_firs_traversal(node):
+  #   result = []
+  #   nodes_to_visit = [node]
+
+  #   while len(nodes_to_visit) > 0:
+  #     node = nodes_to_visit.pop(0)
+  #     result.append(node['value'])
+  #     nodes_to_visit = nodes_to_visit + nodes['children']
+
+  #   return result
+
+  # def depth_first_traversal(node):
+  #   result = []
+  #   nodes_to_visit = [node]
+
+  #   while len(nodes_to_visit) > 0:
+  #     node = nodes_to_visit.pop(0)
+  #     result.append(node['value'])
+  #     nodes_to_visit = node['children'] + nodes_to_visit
+  #   return result  
+
+    #ALTERNATIVE  recursive SOLUTION: 
+  #def depth_first_traversal(node, result = []):
+  # visit each node (add it to the list of results)
+  # result.append(node['value'])
+  # for child in node['children']:
+  #   # visit each child node
+  #   depth_first_traversal(child, result)
+
+  # return result  
